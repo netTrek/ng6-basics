c2c78801f781d1d702fa2cc084a1bea1e0a755af
Saban Uenlue - netTrek <us@netTrek.de>
2018-06-20 13:27:41 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pta-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'pta';
+}
