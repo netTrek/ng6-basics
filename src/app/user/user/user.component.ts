0c0b2f1b9fefc4f8f1fc14bf15125dd2178549d2
Saban Uenlue - netTrek <us@netTrek.de>
2018-06-21 13:39:49 +0200

    003_viewChild

--- src/app/user/user/user.component.ts
+++ src/app/user/user/user.component.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren, ViewEncapsulation } from '@angular/core';
+import { UserAvatarComponent } from './user-avatar/user-avatar.component';
 
 @Component ( {
-  selector: 'pta-user',
+  selector   : 'pta-user',
   templateUrl: './user.component.html',
-  styleUrls: [ './user.component.scss' ]
-})
-export class UserComponent {
+  styleUrls  : [ './user.component.scss' ]/*,
+  encapsulation: ViewEncapsulation.None*/
+} )
+export class UserComponent implements OnInit, AfterViewInit {
   name = 'saban ünlü';
+
+  @ViewChild( 'btn' ) // selector === referenzname im Template
+  btn: ElementRef<HTMLButtonElement>;
+
+  @ViewChild ( UserAvatarComponent )
+  userAvatar: UserAvatarComponent;
+
+  @ViewChildren ( UserAvatarComponent )
+  userAvatars: QueryList<UserAvatarComponent>;
+
   onClick () {
     this.name = 'angular rocks';
   }
+
+  ngAfterViewInit (): void {
+    console.log ( this.userAvatar,
+      this.userAvatars.toArray() );
+    console.log ( this.btn );
+  }
+
+  ngOnInit (): void {
+  }
+
+
 }
