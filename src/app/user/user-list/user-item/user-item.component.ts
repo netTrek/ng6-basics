50e40ee87ecd7c792550ab38fe926bf19b9f5119
Saban Uenlue - netTrek <us@netTrek.de>
2018-08-16 09:35:47 +0200

    003-style

--- src/app/user/user-list/user-item/user-item.component.ts
+++ src/app/user/user-list/user-item/user-item.component.ts
@@ -1,9 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'dg-user-item',
   templateUrl: './user-item.component.html',
+  // template: `<strong>user</strong>
+  // <hr>`,
   styleUrls: ['./user-item.component.scss']
+  // styles: [`h3 {
+  //   color: red;
+  // }`],
+  // encapsulation: ViewEncapsulation.None
 })
 export class UserItemComponent implements OnInit {
 
